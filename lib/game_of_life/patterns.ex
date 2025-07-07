b907a2f94ca7d791badc55112a5866a258d642d2
Steven C <stevencch99@gmail.com>
2025-07-07 17:27:12 +0800

    WIP - Testing new preview patterns

--- lib/game_of_life/patterns.ex
+++ lib/game_of_life/patterns.ex
@@ -18,18 +18,24 @@ defmodule GameOfLife.Patterns do
 
   # Patterns listed in the preview section (Life Lexicon)
   @preview_pattern_ids [
+    :phoenix,
+    :p24_gun,
+    :p60_gun,
+    :rats,
     :tower,
     :glider,
     :block,
-    :beehive,
-    :loaf,
-    :blinker,
-    :toad,
-    :beacon,
-    :acorn,
-    :puffer,
-    :r_pentomino,
-    :glider_gun
+    :beehive
+
+    # WIP：測試畫面中，暫時註解掉其他 id
+    # :loaf,
+    # :blinker,
+    # :toad,
+    # :beacon,
+    # :acorn,
+    # :puffer,
+    # :r_pentomino,
+    # :glider_gun
   ]
 
   @doc """
@@ -96,6 +102,58 @@ defmodule GameOfLife.Patterns do
   @spec all_patterns() :: [Pattern.t()]
   def all_patterns do
     [
+      %Pattern{
+        id: :phoenix,
+        name: "Phoenix",
+        description:
+          ~S'Any pattern all of whose cells die in every generation, but which never dies as a whole. A spaceship cannot be a phoenix, and in fact every finite phoenix eventually evolves into an oscillator. The following 12-cell oscillator (found by the MIT group in December 1971) is the smallest known phoenix, and is sometimes called simply "the phoenix".',
+        category: :oscillators,
+        cells: rle_to_cells("4bo3b$2bobo$6bo$2o$6b2o$bo$3bobo$3bo!")
+      },
+      %Pattern{
+        id: :p24_gun,
+        name: "p24 gun",
+        description:
+          "A true period 24 glider gun that was found by Noam Elkies in 1997 (originally with two different fountains in place of the superfountains used use here). In the form displayed here, it has period 48 due to the filter being used to double the period of the output stream. Remove that bottom-most oscillator to recover the p24 gun.",
+        category: :glider_guns,
+        cells:
+          rle_to_cells("26bo2bo$24b6o$20b2obo8bo$16b2obobobob8o2bo$14b3ob2o3bobo7b3o$13bo6bo2b
+o3b2ob3o$14b4o3bob4o5bob2o$15bo5bo5bo5bobo$13bo3bo4bob2o2bo4b2o7bo$13b
+4o2b4obo4b2o2bo7b2o$18b2o4bo6b3o8bo5bo$15b3o3b3o5bo8bob2o4b2obo$14bo5b
+2o5bob2o7bo3bob4o2bo$9bo5b3o8b2obob3o3bob2o3bobo4bo$9b3o5b2ob2o9b3o6bo
+3bobob3o$3bo18bo5b2o2b2o4bo2bo3bobo$2bobo5b4o4bo6bobo9bo3bo5bob2o$bob
+2ob2obo2b2o5bob2o3bo11bo2bo4b2o2bo$o2bo3bob2o27bo4b2o3b2o$b3obobob2o2b
+2o15bo3bo3bo6b3o$4bobobobo17b2o8bo4b2o3b2o$b2obo3b2o3bobo14bo7bo2bo4b
+2o2bo$bo2b2o2b2o2bo2bo5bobo13bo3bo5bob2o$2b2o3b2obo3b3o3bo2bo14bo2bo3b
+obo$4b3o7b3o4bobo3bo12bo3bobob3o$2b2o3b2obo3b3o11b2o7bob2o3bobo4bo$bo
+2b2o2b2o2bo2bo11b2o9bo3bob4o2bo$b2obo3b2o3bobo22bob2o4b2obo$4bobobobo
+13b2o16bo5bo$b3obobob2o2b2o9bo16b2o$o2bo3bob2o14b3o5bo8bo$bob2ob2obo2b
+2o13bo6b2o$2bobo5b4o19b2o$3bo$9b3o$9bo15bo3bo$24bobobobo8bo$23bo2bobo
+2bo8b2o$22b2ob2ob2ob2o6b2o$21bo3bo3bo3bo$20bo2bob2ob2obo2bo$23bo7bo$
+25b2ob2o$20bo5bobo5bo$21b2ob2o3b2ob2o4$51bo$52b2o$51b2o10$63bo$64b2o$
+63b2o!")
+      },
+      %Pattern{
+        id: :p60_gun,
+        name: "p60 gun",
+        description: "A period 60 gun constructed from two Gosper glider guns.",
+        category: :glider_guns,
+        cells: rle_to_cells("30bo$29bobo$12b2o15b2obo$12bobo14b2ob2o3b2o$3b2o2b2o6bo13b2obo4b2o$3b
+2obo2bo2bo2bo13bobo$7b2o6bo8bo5bo$12bobo7bobo$12b2o9b2o3$25b3o$27bo$
+26bo4bo$32bo$30b3o$9bobo$7bo3bo5b3o$2o5bo7bobo2bo2b2o$2o4bo4bo7b2o2bo
+2bo$7bo19bo$7bo3bo2b3o10bo$9bobo15bo$23bo2bo6b2o$23b2o8bobo$35bo$35b2o
+2$46bo$47bo$45b3o13$61bo$62bo$60b3o!")
+      },
+      %Pattern{
+        id: :rats,
+        name: "Rats",
+        description:
+          "A period 6 oscillator found in 1972. www.conwaylife.com/wiki/index.php?title=$rats",
+        category: :oscillators,
+        cells:
+          rle_to_cells("5b2o5b$6bo5b$4bo7b$2obob4o3b$2obo5bobo$3bo2b3ob2o$3bo4bo3b$4b3obo3b$7b
+o4b$6bo5b$6b2o!")
+      },
       %Pattern{
         id: :glider,
         name: "Glider",
@@ -203,7 +261,7 @@ defmodule GameOfLife.Patterns do
 
   @spec beehive() :: MapSet.t()
   def beehive do
-    MapSet.new([{0, 1}, {1, 0}, {1, 2}, {2, 1}, {3, 1}, {2, 2}])
+    rle_to_cells("b2ob$o2bo$b2ob!")
   end
 
   @spec loaf() :: MapSet.t()
